49eb8035be1cb5b61a23a68b687c0ce4c075a3c5
albaparticle <palagpatski@gmail.com>
2021-09-25 21:44:13 +0800

    Add script to add rotate classes

--- js/bootstrap-wwm-base.js
+++ js/bootstrap-wwm-base.js
@@ -1,10 +1,32 @@
 (function($, Drupal) {
   /**
-   * Sticky header
+   * Add Rotate Classes
    */
   Drupal.behaviors.rotateClass = {
     attach: function(context, settings) {
-      $("figure").children('[class*="16x9_rotate_left"]').addClass("landscape rotate-left");
+      $("figure", context).addClass(function() {
+        let classes = '';
+        if ($(this).has('div[data-entity-embed-display-settings*="16x9"]')) {
+          classes += "landscape-16x9 ";
+        }
+        if ($(this).has('div[data-entity-embed-display-settings*="4x3"]')) {
+          classes += "landscape-4x3 ";
+        }
+        if ($(this).has('div[data-entity-embed-display-settings*="9x16"]')) {
+          classes += "portrait-9x16 ";
+        }
+        if ($(this).has('div[data-entity-embed-display-settings*="3x4"]')) {
+          classes += "portrait-3x4 ";
+        }
+        if ($(this).has('div[data-entity-embed-display-settings*="rotate_left"]')) {
+          classes += "rotate-left ";
+        }
+        if ($(this).has('div[data-entity-embed-display-settings*="rotate_right"]')) {
+          classes += "rotate-right ";
+        }
+
+        return classes;
+      });
     }
   };
 })(jQuery, Drupal);
