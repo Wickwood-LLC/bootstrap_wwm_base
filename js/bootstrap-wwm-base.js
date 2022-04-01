bfb49ac37192b29d86ca474f98fd04f10b381a3f
albaparticle <palagpatski@gmail.com>
2022-04-02 00:20:31 +0800

    Js fix for wlar layouts

--- js/bootstrap-wwm-base.js
+++ js/bootstrap-wwm-base.js
@@ -43,4 +43,16 @@
       }).resize();
     }
   };
+
+  /**
+   * Switch column order in Two-column "wlar" layouts
+   */
+  Drupal.behaviors.wlarFix = {
+    attach: function(context, settings) {
+      $leftColumn = $(".wlar > .layout__region--second", context);
+      $leftColumn.prependTo($leftColumn.parent());
+      console.log($(this));
+    }
+  };
+
 })(jQuery, Drupal);
