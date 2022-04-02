165921e3d99822263730e8a0f234ccc54c5bcdf5
albaparticle <palagpatski@gmail.com>
2022-04-02 23:00:45 +0800

    Removing the pseudoelement two column-wrapping method

--- js/bootstrap-wwm-base.js
+++ js/bootstrap-wwm-base.js
@@ -1,18 +1,5 @@
 (function($, Drupal) {
   /**
-   * Add wrapped column height property
-   */
-  Drupal.behaviors.wrapColumnHeight = {
-    attach: function(context, settings) {
-      $column = $(".wlar > .layout__region--second");
-      $(window).on("resize", function() {
-        let columnHeight = $column.outerHeight() + "px";
-        $(":root").css("--column-height", columnHeight);
-      }).resize();
-    }
-  };
-
-  /**
    * Switch column order in Two-column "wlar" layouts
    */
   Drupal.behaviors.wlarFix = {
