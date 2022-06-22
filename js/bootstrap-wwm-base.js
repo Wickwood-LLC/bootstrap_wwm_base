2d5bfd5ea221bb26601c07c1ad79e94f56e74cce
albaparticle <palagpatski@gmail.com>
2022-06-23 00:41:44 +0800

    Prevent layout switching in Layout Builder form

--- js/bootstrap-wwm-base.js
+++ js/bootstrap-wwm-base.js
@@ -1,12 +1,12 @@
 (function($, Drupal) {
-  /**
-   * Switch column order in Two-column "wlar" layouts
-   */
-  Drupal.behaviors.wlarFix = {
-    attach: function(context, settings) {
-      $leftColumn = $(".wlar > .layout__region--second", context);
-      $leftColumn.prependTo($leftColumn.parent());
-    }
-  };
+    /**
+     * Switch column order in Two-column "wlar" layouts
+     */
+    Drupal.behaviors.wlarFix = {
+        attach: function(context, settings) {
+            $leftColumn = $(".wlar:not(.layout-builder__layout) > .layout__region--second", context);
+            $leftColumn.prependTo($leftColumn.parent());
+        }
+    };
 
 })(jQuery, Drupal);
